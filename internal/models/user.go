0f9b47ab3b8f618f582762007afc49151d566edf
Vik <vikentsi123@gmail.com>
2023-05-25 17:50:04 +0300

    added detailed docs for user

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,7 +4,7 @@ import "time"
 
 // User godoc
 // User represents user data
-// @Schema
+// @Schema(name="User")
 // @ID User
 // @Required id screen_name email password
 // @Optional company_id rank created_at updated_at deleted_at
@@ -18,13 +18,45 @@ import "time"
 // @Property updated_at string "UpdatedAt" format(date-time) example("2023-05-18T18:57:39Z")
 // @Property deleted_at string "DeletedAt" format(date-time)
 type User struct {
-	ID         uint       `json:"id" db:"id" gorm:"primaryKey"`
-	ScreenName string     `json:"screen_name" db:"screen_name"`
-	Email      string     `json:"email" db:"email" gorm:"unique;not null"`
-	Password   string     `json:"password" db:"password"`
-	CompanyId  *int       `json:"company_id" db:"company_id"`
-	Rank       *int       `json:"rank" db:"rank"`
-	CreatedAt  time.Time  `json:"created_at" db:"created_at"`
-	UpdatedAt  time.Time  `json:"updated_at" db:"updated_at"`
-	DeletedAt  *time.Time `json:"deleted_at" db:"deleted_at" sql:"index"`
+	// The auto-created unique identifier for the user.
+	// @Example 123
+	ID uint `json:"id" db:"id" gorm:"primaryKey"`
+
+	// The desired screenname for the user.
+	// This field is required and must contain at least 4 characters.
+	// @Example jonhDoe123
+	ScreenName string `json:"screen_name" db:"screen_name"`
+
+	// The email address of a user.
+	// This field is unique and not-null.
+	// @Format email
+	// @Example john@example.com
+	Email string `json:"email" db:"email" gorm:"unique;not null"`
+
+	// A secret that only the creator of the record knows
+	// @Example 1234567
+	Password string `json:"password" db:"password"`
+
+	// A link to the user's company ID, nullable.
+	// @Example 1
+	CompanyId *int `json:"company_id" db:"company_id"`
+
+	// Status of a user, nullable.
+	// @Example 1
+	Rank *int `json:"rank" db:"rank"`
+
+	// When record was created.
+	// @Format format(date-time)
+	// @Example 2023-05-18T18:57:39Z
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+
+	// When record was updated last time.
+	// @Format format(date-time)
+	// @Example 2023-05-18T18:57:39Z
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+
+	// When record was attempted to delete, nullable.
+	// @Format format(date-time)
+	// @Example 2023-05-18T18:57:39Z
+	DeletedAt *time.Time `json:"deleted_at" db:"deleted_at" sql:"index"`
 }
--- internal/schemas/request_create_user.go
+++ internal/schemas/request_create_user.go
@@ -5,16 +5,16 @@ package schemas
 // @Schema(name="CreateUserRequest")
 // @Required screen_name email password
 // @Description This request includes the necessary details to create a new user.
-// @Example {"screen_name": "exampleUser", "email": "user@example.com", "password": "1234567", "company_id": 1, "rank": 1}
+// @Example {"screen_name": "jonhDoe123", "email": "user@example.com", "password": "1234567", "company_id": 1, "rank": 1}
 type CreateUserRequest struct {
 	// The desired username for the new user. This field is required and must contain at least 4 characters.
 	// @MinLength 4
-	// @Example exampleUser
+	// @Example jonhDoe123
 	ScreenName string `json:"screen_name,omitempty" validate:"required,min=4"`
 
 	// The email address of the new user. This field is required and must be a valid email address.
 	// @Format email
-	// @Example user@example.com
+	// @Example john@example.com
 	Email string `json:"email,omitempty" validate:"required,email"`
 
 	// The password for the new user. This field is required and must contain at least 7 characters.
