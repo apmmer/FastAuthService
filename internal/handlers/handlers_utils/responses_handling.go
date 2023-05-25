8f366da5e001dc59f093f84e6c66f1e90ee905e9
Vik <vikentsi123@gmail.com>
2023-05-25 23:07:05 +0300

    dockerignore edition, response handler edtiion

--- internal/handlers/handlers_utils/responses_handling.go
+++ internal/handlers/handlers_utils/responses_handling.go
@@ -10,9 +10,14 @@ import (
 // HandleJsonResponse converts object to a json server response
 func HandleJsonResponse(w http.ResponseWriter, data interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
+	v := reflect.ValueOf(data)
+	// If data is a pointer, dereference it
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+		data = v.Interface()
+	}
 
 	// Empty slice checking:
-	v := reflect.ValueOf(data)
 	if v.Kind() == reflect.Slice && v.IsNil() {
 		data = reflect.MakeSlice(v.Type(), 0, 0).Interface()
 	}
