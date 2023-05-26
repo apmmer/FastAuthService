a5857867e681cf8252bd68168068f374fd7ae5df
Vik <vikentsi123@gmail.com>
2023-05-26 09:55:34 +0300

    dockerignore edition and postgres entrypoint

--- auth_service_postgres/entrypoint.sh
+++ auth_service_postgres/entrypoint.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-# set -e
-
-# Append SSL config to the postgresql.conf file
-# echo "ssl = on" >> "$PGDATA/postgresql.conf"
-# echo "ssl_cert_file = '/var/lib/postgresql/server.crt'" >> "$PGDATA/postgresql.conf"
-# echo "ssl_key_file = '/var/lib/postgresql/server.key'" >> "$PGDATA/postgresql.conf"
-
-
-# exec docker-entrypoint.sh postgres
-
 set -e
-# Копирование сертификата и ключа из директории certificates
+# Copying the certificate and key from the certificate directory
 echo "ENTRYPOINT: Copying certificates..."
 cp /var/lib/postgresql/certificates/server.crt /var/lib/postgresql/server.crt
 cp /var/lib/postgresql/certificates/server.key /var/lib/postgresql/server.key
 
-# Установите права доступа для файла ключа сертификата
+# Setting the access rights for the certificate and key file
 echo "ENTRYPOINT: Setting permissions..."
 chmod 600 /var/lib/postgresql/server.key
 chmod 644 /var/lib/postgresql/server.crt
