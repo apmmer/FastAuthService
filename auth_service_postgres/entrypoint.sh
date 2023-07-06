f6863b0aa95ed071593e31afaae5a93da6b53f06
Vik <vikentsi123@gmail.com>
2023-07-06 22:26:14 +0300

    entrypoint edition, fixing endline \ r

--- auth_service_postgres/entrypoint.sh
+++ auth_service_postgres/entrypoint.sh
@@ -1,20 +1,12 @@
-#!/bin/bash
 set -e
 
-# Copying the certificate and key from the certificate directory
-# to avoid any kind of access conflicts (if there are volumes etc.)
-echo "ENTRYPOINT: Copying certificates..."
 cp /var/lib/postgresql/certificates/server.crt /var/lib/postgresql/server.crt
 cp /var/lib/postgresql/certificates/server.key /var/lib/postgresql/server.key
 
-# Setting the access rights for the certificate and key file
-echo "ENTRYPOINT: Setting permissions..."
 chmod 600 /var/lib/postgresql/server.key
 chmod 644 /var/lib/postgresql/server.crt
-echo "ENTRYPOINT: Setting user-postgres..."
+
 chown postgres:postgres /var/lib/postgresql/server.key
 chown postgres:postgres /var/lib/postgresql/server.crt
-echo "ENTRYPOINT: Executing command..."
 
-# Calling standard docker-entrypoint.sh
 exec docker-entrypoint.sh postgres
